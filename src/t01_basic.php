13a7b0e0d2648830b12c50d29f8105346944f341
marcpre <marcus.presich@gmail.com>
2019-12-30 21:39:41 +0100

    search unsplash for photos

---
+++ src/t01_basic.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Crew\Unsplash\HttpClient;
+use Crew\Unsplash\Search;
+use Dotenv\Dotenv;
+
+// load env variables
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$accessKey = getenv('ACCESS_KEY');
+$secretKey = getenv('SECRET_KEY');
+
+/**
+ * Search Unsplash for photos
+ * @param array $accessKey
+ * @param array $secretKey
+ */
+function searchUnsplashPhotos(array $accessKey, array $secretKey, $search, $orientation = 'landscape')
+{
+    HttpClient::init([
+        'applicationId' => $accessKey,
+        'secret' => $secretKey,
+        'callbackUrl' => '',
+        'utmSource' => 'Next Return'
+    ]);
+
+    $httpClient = new HttpClient();
+
+
+    $page = 3;
+    $per_page = 15;
+
+    $photos = Search::photos($search, $page, $per_page, $orientation);
+    return $photos;
+}
+
+searchUnsplashPhotos($accessKey, $secretKey);
